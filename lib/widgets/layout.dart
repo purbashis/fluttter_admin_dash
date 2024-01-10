9ce9665f6bcd0969401ff1220609c59648b9b46b
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2024-01-10 18:59:28 +0530

    top_navigation
    added top navigation part but we have to work on drawer

--- lib/widgets/layout.dart
+++ lib/widgets/layout.dart
@@ -2,16 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:fluttter_admin_dash/helpers/responsiveness.dart';
 import 'package:fluttter_admin_dash/widgets/large_screen.dart';
 import 'package:fluttter_admin_dash/widgets/small_screen.dart';
+import 'package:fluttter_admin_dash/widgets/top_nav.dart';
 
 class SiteLayout extends StatelessWidget {
   const SiteLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final  GlobalKey<ScaffoldState>scafflodKey = GlobalKey();
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
+      key: scafflodKey,
+      appBar: topNavigationBar(context, scafflodKey),
+      drawer: Drawer(
+
       ),
       body: ResponsiveWidget(largeScreen: LargeScreen(),  smallScreen: SmallScreen(),)
     );
---
+++ lib/widgets/top_nav.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:fluttter_admin_dash/constants/style.dart';
+import 'package:fluttter_admin_dash/helpers/responsiveness.dart';
+import 'package:fluttter_admin_dash/widgets/custom_text.dart';
+
+AppBar topNavigationBar(BuildContext context, GlobalKey<ScaffoldState> key) =>
+    AppBar(
+      leading: !ResponsiveWidget.isSmallScreen(context)
+          ? Row(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(left: 14),
+                  child: Image.asset(
+                    "assets/icons/logo.png",
+                    width: 28,
+                  ),
+                )
+              ],
+            )
+          : IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                //Row
+                key.currentState?.openDrawer();
+              }),
+      elevation: 0,
+      title: Row(
+        children: [
+          const Visibility(
+            child: CustomText(
+              text: "Dash",
+              color: lightGrey,
+              size: 20,
+              weight: FontWeight.bold,
+            ),
+          ),
+          Expanded(child: Container()),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.settings,
+              color: dark.withOpacity(.7),
+            ),
+          ),
+          Stack(
+            children: [
+              IconButton(
+                  onPressed: () {}, icon: const Icon(Icons.notifications)),
+              Positioned(
+                  top: 7,
+                  right: 7,
+                  child: Container(
+                    width: 12,
+                    height: 12,
+                    padding: const EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                      color: active,
+                      borderRadius: BorderRadius.circular(30),
+                      border: Border.all(color: light, width: 2),
+                    ),
+                  ))
+            ],
+          ),
+          Container(
+            width: 1,
+            height: 22,
+            color: lightGrey,
+          ),
+          const SizedBox(
+            width: 24,
+          ),
+          const CustomText(
+            text: "Purbashis dntz",
+            color: lightGrey,
+          ),
+          const SizedBox(
+            width: 16,
+          ),
+          Container(
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(30)),
+            child: Container(
+              padding: const EdgeInsets.all(2),
+              margin: const EdgeInsets.all(2),
+              child: const CircleAvatar(
+                backgroundColor: light,
+                child: Icon(
+                  Icons.person_outline,
+                  color: dark,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+      iconTheme: IconThemeData(color: dark),
+      backgroundColor: Colors.transparent,
+    );
